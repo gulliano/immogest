e8ea78cf004ac319a7a904ec290d86a0da933531
gulliano <ggulliano@msn.com>
2024-06-25 12:22:32 -0400

    migrate data (user, category, bien)

---
+++ database/migrations/2024_06_25_160247_create_biens_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('biens', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->string('name',60);
+            $table->text('description')->nullable();
+            $table->float('price')->nullable(); 
+            $table->string('city',60)->nullable();
+            $table->string('image',60)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('biens');
+    }
+};
